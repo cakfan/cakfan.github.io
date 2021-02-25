29766962b181fb81d332457c3073554dd0a2b0b5
Taufan Fatahillah <taufan.fhm8@gmail.com>
2021-02-25 14:18:15 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
-$.ajax({
-    url: 'https://www.instagram.com/demi.demik/?__a=1',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    type: "GET", /* or type:"GET" or type:"PUT" */
-    dataType: "json",
-    data: {
-    },
-    success: function (result) {
-        var nama = result.graphql.user.full_name
-        var imgUrl = result.graphql.user.profile_pic_url
-        var text = result.graphql.user.edge_followed_by.count
-        var formated = String(text).replace(/(.)(?=(\d{3})+$)/g,'$1.')
-        document.getElementById('jumlah_pengikut').innerHTML = formated
-        document.getElementById('nama_lengkap').innerHTML = nama
-        document.getElementById('img_profile').src = imgUrl
-        console.log(nama+"\n"+formated);
-    },
-    error: function () {
-        console.log("error");
-    }
-});
+setInterval(function(){
+    $.ajax({
+        url: 'https://www.instagram.com/demi.demik/?__a=1',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        type: "GET", /* or type:"GET" or type:"PUT" */
+        dataType: "json",
+        data: {
+        },
+        success: function (result) {
+            var nama = result.graphql.user.full_name
+            var imgUrl = result.graphql.user.profile_pic_url
+            var text = result.graphql.user.edge_followed_by.count
+            var formated = String(text).replace(/(.)(?=(\d{3})+$)/g,'$1.')
+            document.getElementById('jumlah_pengikut').innerHTML = formated
+            document.getElementById('nama_lengkap').innerHTML = nama
+            document.getElementById('img_profile').src = imgUrl
+            console.log(nama+"\n"+formated);
+        },
+        error: function () {
+            console.log("error");
+        }
+    });
+}, 10000)
 
 // var getJSON = function(url, callback) {
 // var xhr = new XMLHttpRequest();
