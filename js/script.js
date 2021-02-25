2fab1e7e9fdc98b0840697885774c6f3e4d2cc04
Taufan Fatahillah <taufan.fhm8@gmail.com>
2021-02-25 13:50:26 +0700

    update InstaCount ajax

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
-// var ambilJSON = function(url, callback){
-//     fetch('https://www.instagram.com/demi.demik/?__a=1', {
-//   method: 'POST',
-//   headers: { 'Content-Type': 'application/json'}
-// })
-// .then(res => res.json())
-// .then((out) => {
-
-// }).catch(err => console.error(err));
-// }
+$.ajax({
+    url: 'https://www.instagram.com/demi.demik/?__a=1',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    type: "GET", /* or type:"GET" or type:"PUT" */
+    dataType: "json",
+    data: {
+    },
+    success: function (result) {
+        console.log(result);
+    },
+    error: function () {
+        console.log("error");
+    }
+});
 
 var getJSON = function(url, callback) {
 var xhr = new XMLHttpRequest();
@@ -29,19 +35,19 @@ xhr.onload = function() {
 xhr.send();
 };
 
-setInterval(function(){ 
-    getJSON('https://www.instagram.com/demi.demik/?__a=1',  function(err, data) {
-    if (err != null) {
-        console.error(err);
-    } else {
-        var nama = data.graphql.user.full_name
-        var imgUrl = data.graphql.user.profile_pic_url
-        var text = data.graphql.user.edge_followed_by.count
-        var formated = String(text).replace(/(.)(?=(\d{3})+$)/g,'$1.')
-        document.getElementById('jumlah_pengikut').innerHTML = formated
-        document.getElementById('nama_lengkap').innerHTML = nama
-        document.getElementById('img_profile').src = imgUrl
-        console.log(formated);
-    }
-    });
-}, 1000);
+// setInterval(function(){ 
+//     getJSON('https://www.instagram.com/demi.demik/?__a=1',  function(err, data) {
+//     if (err != null) {
+//         // console.error(err);
+//     } else {
+//         var nama = data.graphql.user.full_name
+//         var imgUrl = data.graphql.user.profile_pic_url
+//         var text = data.graphql.user.edge_followed_by.count
+//         var formated = String(text).replace(/(.)(?=(\d{3})+$)/g,'$1.')
+//         document.getElementById('jumlah_pengikut').innerHTML = formated
+//         document.getElementById('nama_lengkap').innerHTML = nama
+//         document.getElementById('img_profile').src = imgUrl
+//         // console.log(formated);
+//     }
+//     });
+// }, 1000);
