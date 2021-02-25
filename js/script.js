c1c31fe3f2c3e9605990613b0812e93e61b050ad
Taufan Fatahillah <taufan.fhm8@gmail.com>
2021-02-25 15:05:58 +0700

    update

--- js/script.js
+++ js/script.js
@@ -13,8 +13,8 @@ setInterval(function(){
             var imgUrl = result.graphql.user.profile_pic_url
             var text = result.graphql.user.edge_followed_by.count
             var formated = String(text).replace(/(.)(?=(\d{3})+$)/g,'$1.')
-            document.getElementById('jumlah_pengikut').innerHTML = formated
-            document.getElementById('nama_lengkap').innerHTML = nama
+            // document.getElementById('jumlah_pengikut').innerHTML = formated
+            document.getElementById('nama_lengkap').textContent = nama
             document.getElementById('img_profile').src = imgUrl
             console.log(nama+"\n"+formated);
         },
@@ -23,40 +23,3 @@ setInterval(function(){
         }
     });
 }, 10000)
-
-// var getJSON = function(url, callback) {
-// var xhr = new XMLHttpRequest();
-// xhr.open('GET', url, true);
-// xhr.setRequestHeader('Content-Type', 'application/json');
-// // xhr.responseType = 'json';
-
-// xhr.onload = function() {
-
-//     var status = xhr.status;
-    
-//     if (status == 200) {
-//         callback(null, xhr.response);
-//     } else {
-//         callback(status);
-//     }
-// };
-
-// xhr.send();
-// };
-
-// setInterval(function(){ 
-//     getJSON('https://www.instagram.com/demi.demik/?__a=1',  function(err, data) {
-//     if (err != null) {
-//         // console.error(err);
-//     } else {
-//         var nama = data.graphql.user.full_name
-//         var imgUrl = data.graphql.user.profile_pic_url
-//         var text = data.graphql.user.edge_followed_by.count
-//         var formated = String(text).replace(/(.)(?=(\d{3})+$)/g,'$1.')
-//         document.getElementById('jumlah_pengikut').innerHTML = formated
-//         document.getElementById('nama_lengkap').innerHTML = nama
-//         document.getElementById('img_profile').src = imgUrl
-//         // console.log(formated);
-//     }
-//     });
-// }, 1000);
