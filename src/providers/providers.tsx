b611dbedbaffe270b843a053a880d853fc0f98db
cakfan <taufan.fhm8@gmail.com>
2025-01-26 15:58:16 +0700

    refactor: upgrade Next.js from 14 to 15

--- src/providers/providers.tsx
+++
@@ -1,9 +0,0 @@
-import { NextThemeProvider } from "./next-theme";
-
-export default function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <NextThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
-    </NextThemeProvider>
-  );
-}
