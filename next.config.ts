723f0fa999742c1a9bb2b8dfeaf1e7e5fd754ddd
cakfan <taufan.fhm8@gmail.com>
2025-01-26 16:19:17 +0700

    build: fix GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/cakfan.github.io",
+  assetPrefix: "/cakfan.github.io",
 };
 
 export default nextConfig;
